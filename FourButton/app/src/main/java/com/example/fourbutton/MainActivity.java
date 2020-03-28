eb4f863ee2ccd95dc40f5c7614c78edc02893dff
jcg0721 <62745617+jcg0721@users.noreply.github.com>
2020-03-28 17:51:12 +0900

    Report #01

---
+++ FourButton/app/src/main/java/com/example/fourbutton/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.fourbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1, button2, button3, button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setBackgroundColor(Color.GRAY);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.nate.com/"));
+                startActivity(mIntent);
+            }
+        });
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setBackgroundColor(Color.GREEN);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:911"));
+                startActivity(mIntent);
+            }
+        });
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setBackgroundColor(Color.RED);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
+                startActivity(mIntent);
+            }
+        });
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setBackgroundColor(Color.YELLOW);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
